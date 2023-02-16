038c537b7490ed208207f2b10422e4cf0e338b50
wuguanliang02 <wuguanliang02@meituan.com>
2023-02-17 02:00:51 +0800

    更新数据

--- main.py
+++ main.py
@@ -3,14 +3,19 @@
 import utils
 import logging
 import work_flow
+import stock_monitor
 import settings
 import schedule
 import time
 
 
-def job():
+def job1():
     if utils.is_weekday():
         work_flow.prepare()
+
+def job2():
+    if utils.is_weekday():
+        stock_monitor.prepare()
 
 
 logging.basicConfig(format='%(asctime)s %(message)s', filename='result.log')
@@ -18,11 +23,14 @@ logging.getLogger().setLevel(logging.INFO)
 settings.init()
 
 if settings.config['cron']:
-    EXEC_TIME = "15:10"
-    schedule.every().day.at(EXEC_TIME).do(job)
+    schedule.every().day.at("15:10").do(job1)
+
+    timeinterval = settings.config['monitor']['timeinterval']
+    schedule.every(timeinterval).seconds.do(job2)
 
     while True:
         schedule.run_pending()
         time.sleep(1)
 else:
-    work_flow.prepare()
+    job1()
+    job2()
---
+++ stock_monitor.py
@@ -0,0 +1,46 @@
+# -*- encoding: UTF-8 -*-
+import data_fetcher
+import settings
+import utils
+import akshare as ak
+from wxpusher import WxPusher
+import push
+import logging
+import time
+from datetime import datetime, timedelta
+
+def prepare():
+    if not settings.config['monitor']['enable'] :
+        return
+
+    # 实时行情信息
+    all_data = ak.stock_zh_a_spot_em()
+
+    stocks = settings.config['monitor']['stocks']
+    if len(stocks) == 0 :
+        return
+    
+    for stock in stocks :
+        process(stock,all_data)
+
+
+def process(stock,all_data):
+    code = stock['code']
+    up = stock['up']
+    down = stock['down']
+    stock_data = all_data.loc[(all_data['代码'] == code)].iloc[0]
+    current_price = stock_data['最新价']
+    if up > 0 and current_price >= up :
+        msg = "【个股追踪提醒】\n"
+        msg = '\n{} {}\n当前股价【{}】\n已上涨至预设定价格【{}】\n'.format(stock_data['代码'], stock_data['名称'], stock_data['最新价'], up)
+        push.statistics(msg)
+
+    if down > 0 and current_price <= down :
+        msg = "【个股追踪提醒】\n"
+        msg = '\n{} {}\n当前股价【{}】\n已下跌至预设定价格【{}】\n'.format(stock_data['代码'], stock_data['名称'], stock_data['最新价'], down)
+        push.statistics(msg)
+
+    # 个股信息
+    # stock_individual_info_em_df = ak.stock_individual_info_em(symbol=code)
+    
+    
